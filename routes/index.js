4fcf0cc1f4bca41f2c4235dbb5a4112e9b5cadd3
MaikenPR <438552@rts-365.dk>
2017-09-21 14:52:07 +0200

    Nye js filer med fetch der skal linkes til i html

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 module.exports = (app) => {
     require('./produkt')(app);
+    require('./afspillere')(app);
     //require('./kontakt')(app);
 };
--- routes/produkt.js
+++ routes/produkt.js
@@ -5,5 +5,11 @@ module.exports = function (app) {
         db.query('select * from produkter order by id desc limit 3', function (err, data) {
             res.send(data);
         })
+    }),
+
+    app.get('/produkt_pop', function (req, res) {
+        db.query('select * from produkter where pris > 4000 limit 3', function (err, data) {
+            res.send(data);
+        })
     })
 }
